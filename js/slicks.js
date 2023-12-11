a34d52a37448df3501eceea757b1fb282d18c634
HariDya <151823724+HariDya@users.noreply.github.com>
2023-12-11 13:34:16 +0530

    Update
    Add new files

---
+++ js/slicks.js
@@ -0,0 +1,22 @@
+
+$('.basic-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+      arrows: true,
+      slidesToShow: 1,
+      /* autoplay: true,
+      autoplaySpeed: 3000, */
+      slidesToScroll:1,
+  });
+      
+
+  
+$('.tesiti-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 2000,
+});
+		
+        
